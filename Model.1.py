2d99c20e6b8a318395e2cd2ff50b891c45df8607
Rachel Surtshin <surtshinforrachel@gmail.com>
2017-11-20 09:04:38 -0500

    Changed file names for clairity

--- Model.1.py
+++ Model.1.py
@@ -27,7 +27,7 @@ avail = avail[~avail['commodity'].str.contains('|'.join(insufficient_data))] #re
 #print(avail)
 
 #POPULATION DATA CLEANING
-pop = pd.read_csv('cansim0510062.2016.2.csv', header = 0)
+pop = pd.read_csv('cansim0510062.2016.csv', header = 0)
 pop = pop.drop(['Ref_Date'], axis = 1) #delete date column
 #pop = pop.pivot(columns = 'GEO', values = 'value') #pivot table so that it is indexed by age group and columns correspond to region      index = 'AGE'
 #age_order = ['0 to 4 years', '5 to 9 years', '10 to 14 years', '15 to 19 years', '20 to 24 years', '25 to 29 years', '30 to 34 years', '35 to 39 years', '40 to 44 years', '45 to 49 years', '50 to 54 years', '55 to 59 years', '60 to 64 years', '65 to 69 years', '70 to 74 years', '75 to 79 years', '80 to 84 years', '85 to 89 years', '90 years and over']
